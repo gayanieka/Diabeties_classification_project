76b205a0a2e04550087500b13060aa058e67afff
gayanieka <gayaniekanayake00@gmail.com>
2026-06-05 20:45:21 +0530

    Add initial Jupyter notebook for data loading functionality

--- model_intepretation.py
+++
@@ -1,9 +0,0 @@
-
-import shap 
-
-# Calculate SHAP values for the model
-explainer = shap.TreeExplainer(model_balanced) 
-shap_values = explainer.shap_values(X_test)
-
-# Plot the summary
-shap.summary_plot(shap_values, X_test)
