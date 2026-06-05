30b9511ee82012c4f1efa90b868943d9a4ddda05
gayanieka <gayaniekanayake00@gmail.com>
2026-06-05 20:31:43 +0530

    Refactor code structure for improved readability and maintainability

--- model_intepretation.py
+++ model_intepretation.py
@@ -1,8 +1,8 @@
-from predictive_modeling import X_test
-import shap # type: ignore
+
+import shap 
 
 # Calculate SHAP values for the model
-explainer = shap.TreeExplainer(model_balanced) # type: ignore
+explainer = shap.TreeExplainer(model_balanced) 
 shap_values = explainer.shap_values(X_test)
 
 # Plot the summary
